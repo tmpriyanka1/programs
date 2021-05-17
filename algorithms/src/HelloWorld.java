e516efdb57f6d6d6057219ab00be663f40ed2c6e
tmpriyanka1 <84302328+tmpriyanka1@users.noreply.github.com>
2021-05-17 00:12:39 -0500

    Added Hello World Code (#1)
    * Added Hello World Code
    
    * Added the Documentation

---
+++ algorithms/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  /**
+   * Method to print Hello World
+   * @param args
+   *  String args passed in to the main funtion.
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
