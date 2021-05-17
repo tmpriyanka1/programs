54c90b7907e2e5c3e010dbbbb6218a67cd15eb30
tmpriyanka1 <tatikondamalathipriyanka@gmail.com>
2021-05-17 00:11:12 -0500

    Added the Documentation

--- algorithms/src/HelloWorld.java
+++ algorithms/src/HelloWorld.java
@@ -1,4 +1,9 @@
 public class HelloWorld {
+  /**
+   * Method to print Hello World
+   * @param args
+   *  String args passed in to the main funtion.
+   */
   public static void main(String[] args)
   {
     System.out.println("Hello World");
